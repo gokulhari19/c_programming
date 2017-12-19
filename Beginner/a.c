cd480af2f76ea89f9eb552610c9b6e4eead99228
Hariharan R <34646456+gokulhari19@users.noreply.github.com>
2017-12-19 14:12:57 +0530

    Add files via upload

---
+++ Beginner/a.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+char name='Sachin Ramesh Tendulkar';
+char nickname='Little Master';
+int runs=18426;
+float avg=44.83;
+int matches=463;
+printf("%c\n%c\n%d\n%f\n%d\n",name,nickname,runs,avg,matches);
+}
